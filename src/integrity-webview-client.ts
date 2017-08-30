f18dd9505c56f12ffe8b16a15f7608119539e4b2
Erik Hedenström <erik@hedenstroem.com>
2017-08-30 20:12:35 +0200

    Fixed broken test

--- src/integrity-webview-client.ts
+++ src/integrity-webview-client.ts
@@ -1,4 +1,3 @@
 // Public classes.
-//export { WebviewClientObject } from './services/webview-client-object';
 export { WebviewClientService } from './services/webview-client.service';
 export { ReceiveMessageService } from './services/receive-message.service';
---
+++ tests/services/webview-client.service.spec.ts
@@ -0,0 +1,23 @@
+import { inject, TestBed } from '@angular/core/testing';
+
+import { WebviewClientService } from './../../index';
+
+describe('WebviewClientService', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        WebviewClientService
+      ]
+    });
+  });
+
+  it('init promise',
+    inject([WebviewClientService],
+      (client: WebviewClientService) => {
+        client.init().then((data: any) => expect(data.test).toEqual('data'));
+        (window as any)['com.brickchain.integrity.init']('{"test":"data"}');
+      })
+  );
+
+});
